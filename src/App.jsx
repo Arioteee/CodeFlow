477ac342d95a45b99f76180a853e4ab897deae81
Arioteee <smoke.tm.rec@gmail.com>
2025-11-27 00:09:17 +0700

    refactor: add basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
+
 function App() {
-  return <></>;
+	return (
+		<div className='min-h-screen bg-slate-950 text-white overflow-hidden'>
+			<Navbar />
+			<main>
+				<Hero />
+				<Features />
+				<Pricing />
+				<Testimonials />
+			</main>
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
