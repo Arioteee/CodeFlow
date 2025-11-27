6f12905a0b1ea7a257b5e56c7bc18fbba3253ac6
Arioteee <smoke.tm.rec@gmail.com>
2025-11-27 22:47:10 +0700

    refactor: add component Buttons

---
+++ src/components/buttons/Buttons.jsx
@@ -0,0 +1,15 @@
+export const ButtonToFile = ({ children, activeTab, onClick }) => {
+	return (
+		<button
+			className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border transition-all duration-200 whitespace-nowrap
+                  ${
+						activeTab === children
+							? 'bg-blue-500/30 text-white border-blue-400/20'
+							: 'bg-white/5 text-gray-300 border-white/10 hover:bg-white/10'
+					}`}
+			onClick={onClick}
+		>
+			{children}
+		</button>
+	)
+}
