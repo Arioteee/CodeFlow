b5c4afc2262c8238c402454bec0032077ae5bc44
Arioteee <smoke.tm.rec@gmail.com>
2025-11-27 22:44:37 +0700

    refactor: add component

---
+++ src/components/circlies/Circlies.jsx
@@ -0,0 +1,9 @@
+export const MacCirclies = () => {
+	return (
+		<div className='flex items-center space-x-1 sm:space-x-2 '>
+			<div className='w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-red-500' />
+			<div className='w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-yellow-500' />
+			<div className='w-2 h-2 sm:w-3 sm:h-3 rounded-full bg-green-500' />
+		</div>
+	)
+}
