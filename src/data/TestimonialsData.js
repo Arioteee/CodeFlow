ff494a5d0df0049551d7c48fd0094dc71c609ab6
Arioteee <smoke.tm.rec@gmail.com>
2025-12-01 01:15:19 +0700

    refactor: add data testimonial

---
+++ src/data/TestimonialsData.js
@@ -0,0 +1,27 @@
+import dude from '../../public/images/dude.jpeg'
+import me from '../../public/images/me.jpeg'
+import woman from '../../public/images/women.jpg'
+
+export const testimonials = [
+	{
+		name: 'Matthew But',
+		role: 'Senior Developer | Creator',
+		image: me,
+		content:
+			'This AI-powered development platform has revolutionized how we write code. The intelligent suggestions save us hours every day.',
+	},
+	{
+		name: 'Marcus Rodriguez',
+		role: 'Tech Lead',
+		image: dude,
+		content:
+			'The automated testing and debugging features are game-changers. We ship code faster and with more confidence than ever before.',
+	},
+	{
+		name: 'Emily Watson',
+		role: 'CTO',
+		image: woman,
+		content:
+			"Our development velocity increased by 300% since adopting this platform. It's like having a senior developer pair programming with every team member.",
+	},
+]
