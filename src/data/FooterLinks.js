56f49556f7c3be8dadd482be31b0c0ec334c3b2f
Arioteee <smoke.tm.rec@gmail.com>
2025-12-01 01:53:22 +0700

    refactor: add data FooterLinks

---
+++ src/data/FooterLinks.js
@@ -0,0 +1,6 @@
+export const footerLinks = {
+	Product: ['Features', 'Pricing', 'Security', 'Roadmap', 'Changelog'],
+	Company: ['About', 'Blog', 'Careers', 'Press', 'Partners'],
+	Resources: ['Documentation', 'Help Center', 'Community', 'API Reference', 'Status'],
+	Legal: ['Privacy', 'Terms', 'Cookie Policy', 'Licenses', 'Compliance'],
+}
