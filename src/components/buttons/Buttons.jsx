8841201795b9ff08af8a6b2f62396114ee340740
Arioteee <smoke.tm.rec@gmail.com>
2025-12-02 23:45:17 +0700

    style(Buttons.jsx): edit styles

--- src/components/buttons/Buttons.jsx
+++ src/components/buttons/Buttons.jsx
@@ -1,7 +1,7 @@
 export const ButtonToFile = ({ children, activeTab, onClick }) => {
 	return (
 		<button
-			className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border transition-all duration-200 whitespace-nowrap
+			className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border border-b-0 transition-all duration-200 whitespace-nowrap
                   ${
 						activeTab === children
 							? 'bg-blue-500/30 text-white border-blue-400/20'
