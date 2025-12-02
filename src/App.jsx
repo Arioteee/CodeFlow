2620bd03c814f02a075841ab0c97c7724d20fdb8
Arioteee <smoke.tm.rec@gmail.com>
2025-12-02 21:53:33 +0700

    refactor: edit import order

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { useEffect, useState } from 'react'
+
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Features from './components/Features'
 import Pricing from './components/Pricing'
 import Testimonials from './components/Testimonials'
 import Footer from './components/Footer'
-import { useEffect, useState } from 'react'
 
 function App() {
 	const [scrolled, setScrolled] = useState(false)
